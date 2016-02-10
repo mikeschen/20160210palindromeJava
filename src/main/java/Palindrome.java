8bfe12c6db81fa131214de0afd8ab72b1a87e74e
Michael Schennum and Ryan King <mikeschenphoto@gmail.com>
2016-02-10 13:53:20 -0800

    add fully functional site

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -4,34 +4,40 @@ import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.lang.*;
+import java.io.*;
 
 public class Palindrome {
   public static void main(String[] args) {
-    // String layout = "templates/layout.vtl";
-    //
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/main.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/detector", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/detector.vtl");
-    //   String userStringNumber = request.queryParams("number");
-    //   Integer userNumber = Integer.parseInt(userStringNumber);
-    //   ArrayList<Object> results = new ArrayList<Object>();
-    //   results = PingPong.isPingPong(userNumber);
-    //
-    //   model.put("results", results);
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/main.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
+      String userInput = request.queryParams("userInput");
+      Boolean isPalindrome = palindromeChecker(userInput);
+
+      model.put("isPalindrome", isPalindrome);
+      model.put("results", userInput);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
   public static Boolean palindromeChecker(String userInput) {
+    userInput = userInput.replaceAll("\\s+","");
+    StringBuilder inputToReverse = new StringBuilder(userInput);
+    String reverse = inputToReverse.reverse().toString();
 
-      //code goes here
-    return true;
+    if (userInput.equals(reverse)) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
 }
--- src/test/java/PalindromeTest.java
+++ src/test/java/PalindromeTest.java
@@ -5,8 +5,26 @@ import java.util.ArrayList;
 public class PalindromeTest {
 
   @Test
-  public void palindromeChecker() {
+  public void palindromeChecker_true() {
     Palindrome testPalindrome = new Palindrome();
     assertEquals(true, testPalindrome.palindromeChecker("stats"));
+  }
+
+  @Test
+  public void palindromeChecker_false() {
+    Palindrome testPalindrome = new Palindrome();
+    assertEquals(false, testPalindrome.palindromeChecker("stat"));
+  }
+
+  @Test
+  public void palindromeChecker_numbers() {
+    Palindrome testPalindrome = new Palindrome();
+    assertEquals(true, testPalindrome.palindromeChecker("12344321"));
+  }
+
+  @Test
+  public void palindromeChecker_removeSPaces() {
+    Palindrome testPalindrome = new Palindrome();
+    assertEquals(true, testPalindrome.palindromeChecker("a but tuba"));
   }
 }
